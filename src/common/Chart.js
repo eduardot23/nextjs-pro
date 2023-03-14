5b7828da5a71163c92338de3d399c93f8b4fac55
eduardot23 <eduardojeda.t23@gmail.com>
2023-03-14 14:16:06 -0600

    adicion de una grafica de barrar al dashboard, solucion de error typeError: labels undefined

--- src/common/Chart.js
+++ src/common/Chart.js
@@ -1,8 +1,7 @@
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
-import { Bar } from 'react-chart-2';
-
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Title, Legend);
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 export const Chart = ({ chartData }) => {
   return (
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,27 +1,33 @@
-const people = [
-  {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    department: 'Optimization',
-    role: 'Admin',
-    email: 'jane.cooper@example.com',
-    image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-];
-
-import useFetch from "@/hooks/useFetch";
-import endPoints from "@/services/api";
+import useFetch from "@/hooks/useFetch"
+import endPoints from "@/services/api"
+import { Chart } from "@/common/Chart"
 
 const PRODUCT_LIMIT = 5
 const PRODUCT_OFFSET = 5
 
 export default function Dashboard() {
 
-  const products = useFetch(endPoints.products.getProducts(PRODUCT_LIMIT,PRODUCT_OFFSET))
-  console.log(products)
+  const products = useFetch(endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET))
+
+  const categoryNames = products?.map((product) => product.category);
+  const categoryCount = categoryNames?.map((category) => category.name);
+
+  const countOccurrences = (arr) => arr.reduce((prev, curr) => ((prev[curr] = ++prev[curr] || 1), prev), {});
+
+  const data = {
+    datasets: [
+      {
+        label: 'Categories',
+        data: countOccurrences(categoryCount),
+        borderWidth: 2,
+        backgroundColor: ['#ffbb11', '#c0c0c0', '#50AF95', 'f3ba2f', '#2a71d0'],
+      },
+    ],
+  };
 
   return (
     <>
+      <Chart className="mb-8 mt-2" chartData={data} />
       <div className="flex flex-col">
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
@@ -51,7 +57,7 @@ export default function Dashboard() {
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200">
                   {products?.map((product) => (
-                    <tr key={`product-Item-${product.id}`}>
+                    <tr key={`Product-Item-${product.id}`}>
                       <td className="px-6 py-4 whitespace-nowrap">
                         <div className="flex items-center">
                           <div className="flex-shrink-0 h-10 w-10">
@@ -88,5 +94,5 @@ export default function Dashboard() {
         </div>
       </div>
     </>
-  );
+  )
 }
